613d0d2767011a9e55f918f71efab0369cd04663
rs10thegreat <67965531+rs10thegreat@users.noreply.github.com>
2020-09-17 12:51:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function draw() {
 }
 function keyPressed(){
 	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:100,y:-170});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
 	}
 }
 
